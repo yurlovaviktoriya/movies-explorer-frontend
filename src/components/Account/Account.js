eb28fea3333afbae86bb9250a33e58bd69b723a1
Victoriya Yurlova <yurlovaviktoriya@yandex.ru>
2022-08-11 10:35:59 +0700

    feat: add account component

---
+++ src/components/Account/Account.js
@@ -0,0 +1,14 @@
+import './Account.css';
+
+function Account() {
+  return (
+    <div className="header__account">
+      <p className="header__account-text">
+        <a className="link" href="/profile">Аккаунт</a>
+      </p>
+      <div className="header__account-icon"></div>
+    </div>
+  );
+}
+
+export default Account;
