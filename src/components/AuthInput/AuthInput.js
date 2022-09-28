423cf48fbd9679e32d8a81b5763d1545265b593f
Victoriya Yurlova <yurlovaviktoriya@yandex.ru>
2022-09-28 14:39:31 +0700

    feat: add component AuthInput

--- src/components/AuthInput/AuthInput.js
+++ src/components/AuthInput/AuthInput.js
@@ -1,9 +1,15 @@
 import './AuthInput.css';
 
-function AuthInput() {
+function AuthInput({ labelText, inputType, isInputError }) {
+
+  const inputClasses = isInputError ? "auth__input auth__input_type_error" : "auth__input";
+  const spanClasses = isInputError ? "auth__input-error auth__input-error_visible" : "auth__input-error";
+
   return (
-    <label>
-      <input className="auth-input" type="text"></input>
+    <label className="auth__label">
+      {labelText}
+      <input className={inputClasses} type={inputType} required></input>
+      <span className={spanClasses}>Что-то пошло не так...</span>
     </label>
   );
 }
