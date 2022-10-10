e53f06c332fe67419c91197ed5bcc75aa10a9811
Victoriya Yurlova <yurlovaviktoriya@yandex.ru>
2022-10-10 20:42:11 +0700

    fix: change base_url

--- src/constants/apiSettings.js
+++ src/constants/apiSettings.js
@@ -1,8 +1,7 @@
 export const MOVIES_BASE_URL = 'https://api.nomoreparties.co';
 
 export const mainApiSettings = {
-  // baseUrl: 'https://api.yurlova.diploma.nomoredomains.sbs',
-  baseUrl: 'http://localhost:3000',
+  baseUrl: 'https://api.yurlova.diploma.nomoredomains.sbs',
   headers: {
     'Content-Type': 'application/json'
   },
